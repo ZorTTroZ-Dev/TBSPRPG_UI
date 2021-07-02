58ccf72059ab98293ca40f77d683326eb05d1bde
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-07-01 21:44:35 -0400

    TbspRpg UI RaceConditions
    Refactored and wrote tests for movement component, race conditions on game creation have been fixed.

--- tbsprpg/src/app/components/game/movement/movement.component.spec.ts
+++ tbsprpg/src/app/components/game/movement/movement.component.spec.ts
@@ -1,13 +1,17 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {Adventure} from '../../../models/adventure';
 import {Game} from '../../../models/game';
+import {Route} from '../../../models/route';
 import {v4 as uuidv4} from 'uuid';
 import {MapService} from '../../../services/map.service';
 import {MovementComponent} from './movement.component';
+import {of} from 'rxjs';
+import {SimpleChange} from '@angular/core';
 
 describe('MovementComponent', () => {
   let fixture: ComponentFixture<MovementComponent>;
   let component: MovementComponent;
+  let getRoutesForGame: jasmine.Spy;
   const testAdventure: Adventure = {
     id: uuidv4().toString(),
     name: 'demo'
@@ -17,6 +21,10 @@ describe('MovementComponent', () => {
     adventureid: testAdventure.id,
     userid: uuidv4()
   };
+  const testRoutes: Route[] = [
+    { id: uuidv4().toString(), name: 'route one', content: 'route one' },
+    { id: uuidv4().toString(), name: 'route two', content: 'route two' }
+  ];
 
   // setup the mock service
   const mapService = jasmine.createSpyObj(
@@ -31,6 +39,7 @@ describe('MovementComponent', () => {
       ]
     }).compileComponents();
 
+    getRoutesForGame = mapService.getRoutesForGame.and.returnValue(of(testRoutes));
     fixture = TestBed.createComponent(MovementComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -39,4 +48,46 @@ describe('MovementComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load the routes', fakeAsync(() => {
+    component.game = testGame;
+    component.ngOnChanges({
+      game: new SimpleChange(null, component.game, true)
+    });
+    fixture.detectChanges();
+    tick();
+
+    expect(component.isMovementError).toBeFalse();
+    expect(getRoutesForGame.calls.any()).toBe(true);
+    expect(component.routes.length).toBe(2);
+    component.ngOnDestroy();
+  }));
+
+  it('should display error if routes not loaded in time', fakeAsync(() => {
+    getRoutesForGame = mapService.getRoutesForGame.and.returnValue(of([]));
+    component.game = testGame;
+    component.ngOnChanges({
+      game: new SimpleChange(null, component.game, true)
+    });
+    fixture.detectChanges();
+    tick(10000);
+    fixture.detectChanges();
+
+    expect(component.isMovementError).toBeTrue();
+    expect(fixture.nativeElement.querySelector('.movement-error')).not.toBeNull();
+    component.ngOnDestroy();
+  }));
+
+  it('should display spinner until routes load', fakeAsync(() => {
+    component.game = testGame;
+    component.ngOnChanges({
+      game: new SimpleChange(null, component.game, true)
+    });
+    fixture.detectChanges();
+    tick();
+    expect(fixture.nativeElement.querySelector('.movement-spinner')).not.toBeNull();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.movement-spinner')).toBeNull();
+    component.ngOnDestroy();
+  }));
 });
--- tbsprpg/src/app/components/game/movement/movement.component.ts
+++ tbsprpg/src/app/components/game/movement/movement.component.ts
@@ -2,7 +2,8 @@ import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@an
 import {Game} from '../../../models/game';
 import {MapService} from '../../../services/map.service';
 import {Route} from '../../../models/route';
-import {Subscription} from 'rxjs';
+import {of, Subject, Subscription, timer} from 'rxjs';
+import {catchError, map, switchMap, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-movement',
@@ -13,6 +14,7 @@ export class MovementComponent implements OnInit, OnChanges, OnDestroy {
   @Input() game: Game;
   isMovementError: boolean;
   routes: Route[];
+  routesLoaded: Subject<Route[]>;
   private subscription: Subscription = new Subscription();
 
   constructor(private mapService: MapService) { }
@@ -24,21 +26,35 @@ export class MovementComponent implements OnInit, OnChanges, OnDestroy {
   ngOnInit(): void {
     this.routes = [];
     this.isMovementError = false;
+    this.routesLoaded = new Subject<Route[]>();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     // when the game changes
     // load routes until we get a valid response
 
-    // if (changes.game.currentValue) {
-    //   this.subscription.add(
-    //     this.mapService.getRoutesForGame(this.game.id).pipe(
-    //       switchMap(routes => from(routes))
-    //     ).subscribe(route => {
-    //       this.routes.push(route);
-    //     })
-    //   );
-    // }
+    if (changes.game.currentValue) {
+      this.subscription.add(
+        timer(0, 500).pipe(
+          takeUntil(this.routesLoaded),
+          map(tic => {
+            if (tic >= 20) {
+              this.isMovementError = true;
+              throw new Error('failed to load game movements');
+            }
+            return tic;
+          }), // only try for 20 half seconds which is 10 seconds
+          switchMap(() => this.mapService.getRoutesForGame(this.game.id)),
+          tap(routes => {
+            if (routes !== null && routes.length > 0) {
+              this.routes.push(...routes);
+              this.routesLoaded.next(routes);
+            }
+          }),
+          catchError(() => of(null))
+        ).subscribe()
+      );
+    }
   }
 
 }
