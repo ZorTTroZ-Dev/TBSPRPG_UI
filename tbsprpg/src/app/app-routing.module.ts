7722be052f18414cd8f58a7409892733ccda1b1e
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-09-15 21:18:37 -0400

    UI Admin Interface
    Bootstrapping details. I think I can start making actual progress again.

--- tbsprpg/src/app/app-routing.module.ts
+++ tbsprpg/src/app/app-routing.module.ts
@@ -6,13 +6,12 @@ import { LoginComponent } from './components/mainsite/login/login.component';
 import { GameComponent } from './components/game/game.component';
 import { AuthGuard } from './guards/auth.guard';
 import {AdventuresComponent} from './components/adventures/adventures.component';
-import {AdventureEditComponent} from './components/adventures/adventure-details/adventure-edit/adventure-edit.component';
 import {AdventureCreatorComponent} from './components/adventures/adventure-creator/adventure-creator.component';
+import {AdventureDetailsComponent} from './components/adventures/adventure-details/adventure-details.component';
 
 const routes: Routes = [
   { path: 'adventure', component: AdventuresComponent, canActivate: [AuthGuard] },
-  { path: 'adventure-edit/:adventureId', component: AdventureEditComponent, canActivate: [AuthGuard] },
-  { path: 'adventure-edit', component: AdventureEditComponent, canActivate: [AuthGuard] },
+  { path: 'adventure-details/:adventureId', component: AdventureDetailsComponent, canActivate: [AuthGuard] },
   { path: 'adventure-creator', component: AdventureCreatorComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
