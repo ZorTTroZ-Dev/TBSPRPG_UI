23370ecff9bc8085f5dc2821648f2bac59cdeae4
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-09-17 18:22:38 -0400

    UI Admin Interface
    Cleared out adventure editor.

--- tbsprpg/src/app/components/adventures/adventure-details/adventure-edit/adventure-edit.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/adventure-edit/adventure-edit.component.ts
@@ -1,52 +1,20 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
-import {of, Subscription} from 'rxjs';
-import {map, switchMap} from 'rxjs/operators';
-import {ActivatedRoute} from '@angular/router';
-import {AdventureService} from '../../../../services/adventure.service';
 
 @Component({
   selector: 'app-adventure-edit',
   templateUrl: './adventure-edit.component.html',
   styleUrls: ['./adventure-edit.component.scss']
 })
-export class AdventureEditComponent implements OnInit, OnDestroy {
-  private subscription: Subscription = new Subscription();
+export class AdventureEditComponent implements OnInit {
   adventureForm = new FormGroup({
     id: new FormControl(''),
     name: new FormControl('')
   });
 
-  constructor(private route: ActivatedRoute, private adventureService: AdventureService) { }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.subscription.add(
-      this.route.paramMap.pipe(
-        map(params => {
-          if (params.get('adventureId') === '') {
-            return null;
-          }
-          return params.get('adventureId');
-        }),
-        switchMap(adventureId => {
-          if (adventureId !== null) {
-            return this.adventureService.getAdventureById(adventureId);
-          }
-          return of(null);
-        })
-      ).subscribe(adventure => {
-        if (adventure !== null) {
-          this.adventureForm.setValue(adventure);
-        }
-      })
-    );
-  }
-
-  updateAdventure(): void {
   }
 
 }
