1b9aaa55738ebb85089e2676714da76153f2cb7e
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-09-20 21:32:09 -0400

    UI Admin Interface
    Bootstrapping, trying to design location edit form.

--- tbsprpg/src/app/components/adventures/adventure-details/adventure-details-location-edit/adventure-details-location-edit.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/adventure-details-location-edit/adventure-details-location-edit.component.ts
@@ -1,17 +1,30 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Location} from '../../../../models/location';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-adventure-details-location-edit',
   templateUrl: './adventure-details-location-edit.component.html',
   styleUrls: ['./adventure-details-location-edit.component.scss']
 })
-export class AdventureDetailsLocationEditComponent implements OnInit {
+export class AdventureDetailsLocationEditComponent implements OnInit, OnChanges {
   @Input() location: Location;
+  locationForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('')
+  });
+  // private subscriptions: Subscription = new Subscription();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // the game has changed, get most recent content for display, and start polling
+    if (changes.location.currentValue) {
+      this.locationForm.setValue(this.location);
+    }
   }
 
 }
