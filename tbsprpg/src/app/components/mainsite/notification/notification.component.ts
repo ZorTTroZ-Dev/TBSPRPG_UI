3a55680f9ef3404d116ffc842ad4eb3a60e54960
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-10-06 21:42:02 -0400

    UI Admin Interface
    Working on getting message to pop.

--- tbsprpg/src/app/components/mainsite/notification/notification.component.ts
+++ tbsprpg/src/app/components/mainsite/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NotificationService} from '../../../services/notification.service';
 import {Subscription} from 'rxjs';
+import {Notification} from '../../../models/notification';
 
 @Component({
   selector: 'app-notification',
@@ -9,18 +10,36 @@ import {Subscription} from 'rxjs';
 })
 export class NotificationComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
+  notifications: Notification[];
+  private toastObserver: MutationObserver;
 
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    this.notifications = [];
     this.subscriptions.add(
       this.notificationService.getNotifications().subscribe(notification => {
-        console.log(notification);
+        this.notifications.push(notification);
       })
     );
+
+    this.toastObserver = new MutationObserver((mutationRecords, mutationObserver) => {
+      console.log(mutationRecords);
+      for (const mutation of mutationRecords) {
+        if (mutation.type === 'childList') {
+          console.log(mutation);
+          mutation.addedNodes.forEach((value, key) => {
+            console.log(value);
+          });
+        }
+      }
+    });
+    this.toastObserver.observe(document.getElementById('toaster'), {
+      attributes: true, childList: true, subtree: true});
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+    this.toastObserver.disconnect();
   }
 }
