45410f574311bbb20919e726a0c4cc4f5b6dab4b
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-10-17 20:51:10 -0400

    UI Admin Interface
    Working on editing route source.

--- tbsprpg/src/app/components/adventures/adventure-details/adventure-details-routes-edit/adventure-details-routes-edit.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/adventure-details-routes-edit/adventure-details-routes-edit.component.ts
@@ -1,9 +1,11 @@
 import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {Location} from '../../../../models/location';
-import {Subscription} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
 import {RoutesService} from '../../../../services/routes.service';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Route} from '../../../../models/route';
+import {SourcesService} from '../../../../services/sources.service';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-adventure-details-routes-edit',
@@ -13,12 +15,28 @@ import {Route} from '../../../../models/route';
 export class AdventureDetailsRoutesEditComponent implements OnInit, OnChanges, OnDestroy {
   @Input() location: Location;
   private subscriptions: Subscription = new Subscription();
+  routeLoaded: Subject<Route>;
   routesFormArray: FormGroup[] = [];
   routes: Route[] = [];
 
-  constructor(private routesService: RoutesService, ) { }
+  constructor(private routesService: RoutesService,
+              private sourcesService: SourcesService) {
+    this.routeLoaded = new Subject<Route>();
+  }
 
   ngOnInit(): void {
+    this.subscriptions.add(
+      this.routeLoaded.pipe(
+        tap(route => {
+          this.sourcesService.getSourceForAdventureForKey(
+            this.location.adventureId,
+            route.sourceKey,
+            'en').subscribe(source => {
+            console.log(source);
+          });
+        })
+      ).subscribe()
+    );
   }
 
   ngOnDestroy(): void {
@@ -27,12 +45,14 @@ export class AdventureDetailsRoutesEditComponent implements OnInit, OnChanges, O
 
   addRouteToForm(route: Route): void {
     this.routesFormArray.push(new FormGroup({
+      route: new FormGroup({
         id: new FormControl(route.id),
         name: new FormControl(route.name),
         sourceKey: new FormControl(route.sourceKey),
         successSourceKey: new FormControl(route.successSourceKey),
         locationId: new FormControl(route.locationId),
         destinationLocationId: new FormControl(route.destinationLocationId)
+      })
     }));
   }
 
@@ -43,6 +63,7 @@ export class AdventureDetailsRoutesEditComponent implements OnInit, OnChanges, O
           this.routes = routes;
           routes.forEach(route => {
             this.addRouteToForm(route);
+            this.routeLoaded.next(route);
           });
         })
       );
