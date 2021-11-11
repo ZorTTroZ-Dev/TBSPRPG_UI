7f164d6c156829ce1cce1180efd144f9b451238a
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-11-10 21:04:55 -0500

    UI Admin Interface
    Explorer page gets adventures from the API.

--- tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AdventureService} from '../../../services/adventure.service';
+import {Adventure} from '../../../models/adventure';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-adventure-explorer',
   templateUrl: './adventure-explorer.component.html',
   styleUrls: ['./adventure-explorer.component.scss']
 })
-export class AdventureExplorerComponent implements OnInit {
+export class AdventureExplorerComponent implements OnInit, OnDestroy {
+  adventures: Adventure[];
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private adventureService: AdventureService) { }
 
   ngOnInit(): void {
+    this.subscriptions.add(
+      this.adventureService.getAdventures().subscribe(result => {
+        this.adventures = result;
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
