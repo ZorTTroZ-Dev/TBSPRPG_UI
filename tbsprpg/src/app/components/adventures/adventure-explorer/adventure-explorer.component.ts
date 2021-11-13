07c29421b41b30854a065335a23d8eb50a15c8cf
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-11-12 19:03:33 -0500

    UI Admin Interface
    Tweaks to the adventure explore page.

--- tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AdventureService} from '../../../services/adventure.service';
 import {Adventure} from '../../../models/adventure';
-import {Subscription} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
+import {switchMap, tap} from 'rxjs/operators';
+import {SettingService} from '../../../services/setting.service';
+import {SourcesService} from '../../../services/sources.service';
 
 @Component({
   selector: 'app-adventure-explorer',
@@ -10,14 +13,33 @@ import {Subscription} from 'rxjs';
 })
 export class AdventureExplorerComponent implements OnInit, OnDestroy {
   adventures: Adventure[];
+  adventureSubject: Subject<Adventure>;
+  sourceMap = new Map();
   private subscriptions: Subscription = new Subscription();
 
-  constructor(private adventureService: AdventureService) { }
+  constructor(private adventureService: AdventureService,
+              private sourcesService: SourcesService,
+              private settingService: SettingService) {
+    this.adventureSubject = new Subject<Adventure>();
+  }
 
   ngOnInit(): void {
     this.subscriptions.add(
+      this.adventureSubject.pipe(
+        switchMap(adventure => this.sourcesService.getSourceForAdventureForKey(
+          adventure.id, adventure.sourceKey, this.settingService.getLanguage())),
+        tap(source => {
+          this.sourceMap.set(source.adventureId, source.text);
+        })
+      ).subscribe()
+    );
+
+    this.subscriptions.add(
       this.adventureService.getAdventures().subscribe(result => {
         this.adventures = result;
+        for (const adventure of this.adventures) {
+          this.adventureSubject.next(adventure);
+        }
       })
     );
   }
