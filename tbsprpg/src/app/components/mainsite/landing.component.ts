b2e46e906aabf6a5ee5137c2ff56c3923f938e1b
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-12-16 20:27:39 -0500

    Initial Release
    Can now log in from the landing page.

--- tbsprpg/src/app/components/mainsite/landing.component.ts
+++ tbsprpg/src/app/components/mainsite/landing.component.ts
@@ -1,23 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {Subscription} from 'rxjs';
+import {PERMISSION_ADVENTURE_EDIT} from '../../guards/permission.guard';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit {
-  email: string;
+export class LandingComponent implements OnInit, OnDestroy {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    remember: new FormControl(false)
+  });
+  private subscriptions: Subscription = new Subscription();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
   getStarted(): void {
-    // check if we have an account for this email, use a user service
-    // if so and they're not logged in go to a login screen
-    // else go to a registration page
-    this.router.navigate(['/adventure', {}]);
+    const loginData = this.loginForm.value;
+    this.subscriptions.add(
+      this.userService.authenticate(loginData.email, loginData.password).subscribe(
+        user => {
+          if (user.permissions && user.permissions.includes(PERMISSION_ADVENTURE_EDIT)) {
+            this.router.navigate(['/adventure', {}]);
+          } else {
+            this.router.navigate(['/adventure-explorer', {}]);
+          }
+        }, () => {
+          // authentication failed; either they entered the wrong email address or password
+          this.router.navigate(['/register', {}]);
+        }
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
