c8f5fd097e7d682f54d9fa321620310ec9846943
cdvanhorn <cdavid.vanhorn@gmail.com>
2022-02-01 21:29:45 -0500

    Adventure Create
    Pulled adventure edit form to it's own component.

--- tbsprpg/src/app/app.module.ts
+++ tbsprpg/src/app/app.module.ts
@@ -49,6 +49,7 @@ import {AdRouteEditComponent} from './components/adventures/adventure-details/ad
 import {AdventureEditComponent} from './components/adventures/adventure-edit/adventure-edit.component';
 import {AdventureExplorerComponent} from './components/adventures/adventure-explorer/adventure-explorer.component';
 import {AdGamesComponent} from './components/adventures/adventure-details/ad-games/ad-games.component';
+import {AeFormComponent} from './components/adventures/adventure-edit/ae-form/ae-form.component';
 
 // directives
 import {FocusOnShowDirectiveDirective} from './directives/focus-on-show-directive.directive';
@@ -85,6 +86,7 @@ import {PasswordValidatorDirective} from './directives/password-validator.direct
     RegistrationVerifyComponent,
     LoginFailedComponent,
     PasswordValidatorDirective,
+    AeFormComponent,
   ],
   imports: [
     BrowserModule,
