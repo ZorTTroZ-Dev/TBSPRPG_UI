9fab3adbca15945f05c9a701c2b60a19e74f038d
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-04-16 22:05:36 -0400

    Feature/delete adventure (#9)
    * Update .gitignore
    
    Ignore ide files.
    
    * Delete Adventure
    
    Implemented delete adventure button.
    
    * Delete Adventure
    
    Tested and fixed bugs, adventure table reloads on delete.

--- tbsprpg/src/app/components/adventures/adventures-table/adventures-table.component.ts
+++ tbsprpg/src/app/components/adventures/adventures-table/adventures-table.component.ts
@@ -1,8 +1,12 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {Adventure} from '../../../models/adventure';
 import {AdventureTableTypes} from '../../../view_models/adventure-table-types';
 import {AdventureService} from '../../../services/adventure.service';
 import {UserService} from '../../../services/user.service';
+import {Subject, Subscription} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {Notification, NOTIFICATION_TYPE_SUCCESS} from '../../../models/notification';
+import {NotificationService} from '../../../services/notification.service';
 
 @Component({
   selector: 'app-adventures-table',
@@ -10,30 +14,58 @@ import {UserService} from '../../../services/user.service';
   styleUrls: ['./adventures-table.component.scss']
 })
 
-export class AdventuresTableComponent implements OnInit, OnChanges {
+export class AdventuresTableComponent implements OnInit, OnChanges, OnDestroy {
   @Input() tableType: string;
   adventures: Adventure[];
+  adventureObservable: Subject<string>;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor(private adventureService: AdventureService, private userService: UserService) { }
-
-  ngOnInit(): void {
+  constructor(private adventureService: AdventureService,
+              private userService: UserService,
+              private notificationService: NotificationService) {
     this.adventures = [];
+    this.adventureObservable = new Subject<string>();
+
+    this.subscriptions.add(
+      this.adventureObservable.pipe(
+        map(userId => this.adventureService.getAdventuresCreatedBy(userId)),
+        tap(response => {
+          response.subscribe(adventures => {
+            this.adventures = adventures;
+          });
+        })
+      ).subscribe()
+    );
+  }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     // the game has changed, get most recent content for display, and start polling
     if (changes.tableType.currentValue) {
       if (this.tableType === AdventureTableTypes.CREATED_ADVENTURES) {
-        this.getOwnedAdventures();
+        const userId = this.userService.getUserId();
+        this.adventureObservable.next(userId);
       }
     }
   }
 
-  getOwnedAdventures(): void {
-    const userId = this.userService.getUserId();
-    this.adventureService.getAdventuresCreatedBy(userId).subscribe(adventures => {
-      this.adventures.push(...adventures);
-    });
+  deleteAdventure(adventure: Adventure): void {
+    this.subscriptions.add(
+      this.adventureService.deleteAdventure(adventure).subscribe(() => {
+        const notification: Notification = {
+          type: NOTIFICATION_TYPE_SUCCESS,
+          message: 'adventure deleted'
+        };
+        this.notificationService.postNotification(notification);
+        const userId = this.userService.getUserId();
+        this.adventureObservable.next(userId);
+      })
+    );
   }
 
 }
