9c5e2597f06f9f92ceb485137a398422174481c1
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-04-16 21:51:17 -0400

    MarkDown Source
    Display source in certain situations as HTML.

--- tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-explorer/adventure-explorer.component.ts
@@ -32,7 +32,7 @@ export class AdventureExplorerComponent implements OnInit, OnDestroy {
 
     this.subscriptions.add(
       this.adventureSubject.pipe(
-        switchMap(adventure => this.sourcesService.getSourceForAdventureForKey(
+        switchMap(adventure => this.sourcesService.getProcessedSourceForAdventureForKey(
           adventure.id, adventure.descriptionSourceKey, this.settingService.getLanguage())),
         tap(source => {
           this.sourceMap.set(source.adventureId, source.text);
