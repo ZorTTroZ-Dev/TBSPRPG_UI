a75c05b12197d66878317b5594455b9e8c2eb5a4
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-07-02 20:39:59 -0400

    Feature/TRUI-2 TRUI-3 routes list page (#18)
    * feature/TRUI-2 Routes List
    
    Added page that lists all the routes for an adventure.
    
    * feature/TRUI-2 TRUI-3 Routes List/Delete
    
    Added ability to delete a route.

--- tbsprpg/src/app/components/adventures/adventure-details/ad-routes/ad-routes.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/ad-routes/ad-routes.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Adventure} from '../../../../models/adventure';
+import {Subject, Subscription} from 'rxjs';
+import {Route} from '../../../../models/route';
+import {RoutesService} from '../../../../services/routes.service';
+import {map, tap} from 'rxjs/operators';
+import {NotificationService} from '../../../../services/notification.service';
+import {Notification, NOTIFICATION_TYPE_SUCCESS} from '../../../../models/notification';
 
 @Component({
   selector: 'app-ad-routes',
   templateUrl: './ad-routes.component.html',
   styleUrls: ['./ad-routes.component.scss']
 })
-export class AdRoutesComponent implements OnInit {
+export class AdRoutesComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() adventure: Adventure;
+  routes: Route[];
+  routeObservable: Subject<string>;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private routesService: RoutesService,
+              private notificationService: NotificationService) {
+    this.routes = [];
+    this.routeObservable = new Subject<string>();
+
+    this.subscriptions.add(
+      this.routeObservable.pipe(
+        map(adventureId => this.routesService.getRoutesForAdventure(adventureId)),
+        tap(response => {
+          response.subscribe(routes => {
+            this.routes = routes;
+          });
+        })
+      ).subscribe()
+    );
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.adventure.currentValue) {
+      this.routeObservable.next(this.adventure.id);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  deleteRoute(route: Route): void {
+    this.subscriptions.add(
+      this.routesService.deleteRoute(route).subscribe(() => {
+        const notification: Notification = {
+          type: NOTIFICATION_TYPE_SUCCESS,
+          message: 'route deleted'
+        };
+        this.notificationService.postNotification(notification);
+        this.routeObservable.next(this.adventure.id);
+      })
+    );
+  }
 }
--- tbsprpg/src/app/services/routes.service.ts
+++ tbsprpg/src/app/services/routes.service.ts
@@ -70,4 +70,18 @@ export class RoutesService extends BaseService{
       catchError(this.handleError<any>('updateRoutes', null))
     );
   }
+
+  getRoutesForAdventure(adventureId: string): Observable<Route[]> {
+    return this.http.get<Route[]>(this.routesUrl + '/adventure/' + adventureId)
+      .pipe(
+        catchError(this.handleError<Route[]>('getRoutesForAdventure', null))
+      );
+  }
+
+  deleteRoute(route: Route): Observable<any> {
+    return this.http.delete(this.routesUrl + '/' + route.id)
+      .pipe(
+        catchError(this.handleError<any>('deleteRoute', null))
+      );
+  }
 }
