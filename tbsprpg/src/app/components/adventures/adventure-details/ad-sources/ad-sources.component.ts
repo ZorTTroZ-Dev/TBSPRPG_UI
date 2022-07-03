5759333d52aa71cb86c331e859b325933b1d81a7
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-07-02 20:39:40 -0400

    feature/TRUI-4 Sources List (#17)
    Added page that lists all the sources for an adventure.

--- tbsprpg/src/app/components/adventures/adventure-details/ad-sources/ad-sources.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/ad-sources/ad-sources.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Adventure} from '../../../../models/adventure';
+import {Subject, Subscription} from 'rxjs';
+import {Source} from '../../../../models/source';
+import {SourcesService} from '../../../../services/sources.service';
+import {map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-ad-sources',
   templateUrl: './ad-sources.component.html',
   styleUrls: ['./ad-sources.component.scss']
 })
-export class AdSourcesComponent implements OnInit {
+export class AdSourcesComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() adventure: Adventure;
+  sources: Source[];
+  sourceObservable: Subject<string>;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private sourcesService: SourcesService) {
+    this.sources = [];
+    this.sourceObservable = new Subject<string>();
+
+    this.subscriptions.add(
+      this.sourceObservable.pipe(
+        map(adventureId => this.sourcesService.getAllSourceForAdventure(adventureId)),
+        tap(response => {
+          response.subscribe(sources => {
+            this.sources = sources;
+          });
+        })
+      ).subscribe()
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.adventure.currentValue) {
+      this.sourceObservable.next(this.adventure.id);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
