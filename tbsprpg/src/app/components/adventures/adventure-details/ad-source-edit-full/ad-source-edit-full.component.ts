5a90e1f877d396d193359bc5b7de8ce50e757ede
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-08-02 21:37:35 -0400

    feature/TRUI-6 Create Edit Source
    Working on the source creation/edit form.

--- tbsprpg/src/app/components/adventures/adventure-details/ad-source-edit-full/ad-source-edit-full.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/ad-source-edit-full/ad-source-edit-full.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Source} from '../../../../models/source';
+import {FormGroup} from '@angular/forms';
+import {SourcesService} from '../../../../services/sources.service';
 
 @Component({
   selector: 'app-ad-source-edit-full',
   templateUrl: './ad-source-edit-full.component.html',
   styleUrls: ['./ad-source-edit-full.component.scss']
 })
-export class AdSourceEditFullComponent implements OnInit {
+export class AdSourceEditFullComponent implements OnInit, OnChanges {
+  @Input() source: Source;
+  sourceForm: FormGroup;
 
-  constructor() { }
+  constructor(private sourcesService: SourcesService) { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.source.currentValue) {
+      this.sourceForm = this.sourcesService.createFormGroupForSource(this.source);
+    }
+  }
+
+  updateSource(): void {
+
+  }
 }
