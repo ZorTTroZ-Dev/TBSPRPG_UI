8a42b8af76d28ab1c453a12bb7288922f721a10f
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-08-02 21:17:27 -0400

    feature/TRUI-6 Create Edit Source
    Added page for editing/creating source.

---
+++ tbsprpg/src/app/components/adventures/adventure-details/ad-source-edit-full/ad-source-edit-full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-source-edit-full',
+  templateUrl: './ad-source-edit-full.component.html',
+  styleUrls: ['./ad-source-edit-full.component.scss']
+})
+export class AdSourceEditFullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
