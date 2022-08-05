cffdd9bca9c7c2c860800221748498cf81fc8ba6
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2022-08-04 21:40:36 -0400

    feature/TRUI-7 Create Edit Routes
    Started work on route edit/create form.

--- tbsprpg/src/app/components/adventures/adventure-details/ad-route-edit/ad-route-edit.component.ts
+++ tbsprpg/src/app/components/adventures/adventure-details/ad-route-edit/ad-route-edit.component.ts
@@ -1,15 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {Location} from '../../../../models/location';
 import {RoutesService} from '../../../../services/routes.service';
 import {Script} from '../../../../models/script';
+import {Route} from '../../../../models/route';
 
 @Component({
   selector: 'app-ad-route-edit',
   templateUrl: './ad-route-edit.component.html',
   styleUrls: ['./ad-route-edit.component.scss']
 })
-export class AdRouteEditComponent implements OnInit {
+export class AdRouteEditComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() route: Route;
   @Input() routeForm: FormGroup;
   @Input() locations: Location[];
   @Input() scripts: Script[];
@@ -17,12 +19,23 @@ export class AdRouteEditComponent implements OnInit {
   routeSourceSuccessLabel = 'Route Taken';
   routeSourceFormGroupName: string;
   routeSourceSuccessFormGroupName: string;
+  standalone: boolean;
 
   constructor(private routesService: RoutesService) { }
 
   ngOnInit(): void {
     this.routeSourceFormGroupName = this.routesService.routeSourceFormGroupName;
     this.routeSourceSuccessFormGroupName = this.routesService.routeSourceSuccessFormGroupName;
+    this.standalone = false;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.route.currentValue) {
+      this.standalone = true;
+    }
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
