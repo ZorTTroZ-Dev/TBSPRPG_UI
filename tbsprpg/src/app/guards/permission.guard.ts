9738a5ae528e156f03c2a5b757c4122417759d43
cdvanhorn <cdavid.vanhorn@gmail.com>
2023-06-30 22:12:36 -0400

    feature/Upgrade Angular
    Upgrade to angular 16 and update all other packages.

--- tbsprpg/src/app/guards/permission.guard.ts
+++ tbsprpg/src/app/guards/permission.guard.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import {UserService} from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PermissionGuard implements CanActivate {
+export class PermissionGuard  {
   constructor(private router: Router, private userService: UserService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
--- tbsprpg/src/app/guards/registration-complete.guard.ts
+++ tbsprpg/src/app/guards/registration-complete.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {UserService} from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegistrationCompleteGuard implements CanActivate {
+export class RegistrationCompleteGuard  {
   constructor(private userService: UserService) {}
 
   canActivate(
